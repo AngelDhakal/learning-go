a2e314f78b3e298326d35e04fc5b64be56760a6c
Angel Dhakal <anjaldhakal01@gmail.com>
2020-10-20 21:02:46 +0545

    first go program

---
+++ first.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main1() {
+	fmt.Println("Hello, World")
+}
+
+// to run the program execute "go run first.go" in command line
+
+/*
+to compile a file into a binary executable type "go build first.go" in cmd line
+to run the executable "./first"
+*/
