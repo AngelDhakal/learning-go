26a7b3f72e1d4100738119682eaf71d22bc0f894
Angel Dhakal <anjaldhakal01@gmail.com>
2020-10-31 15:18:47 +0545

    using TypeOf from reflect pkg to find type of a var

---
+++ typeof.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	x := []int{1, 2, 3}
+	y := [3]int{1, 2, 3}
+	z := "angel"
+	aa := 10
+	ab := 10.10
+	fmt.Println(reflect.TypeOf(x))
+	fmt.Println(reflect.TypeOf(y))
+	fmt.Println(reflect.TypeOf(z))
+	fmt.Println(reflect.TypeOf(aa))
+	fmt.Println(reflect.TypeOf(ab))
+}
