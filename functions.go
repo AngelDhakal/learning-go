1d7598f16003d5e5b9aec57fd3479afd366a75a4
Angel Dhakal <anjaldhakal01@gmail.com>
2020-11-01 22:34:44 +0545

    avg func

--- functions.go
+++ functions.go
@@ -6,7 +6,9 @@ func main() {
 	a, b := calc(15, 10)
 	fmt.Println(a, b)
 	fmt.Println("Value Printed")
-
+	x := []int{1, 2, 3, 4, 5}
+	y := sliceAverage(x)
+	fmt.Printf("average is %v\n", y)
 }
 
 func calc(x, y int) (addition int, subtraction int) {
@@ -15,5 +17,14 @@ func calc(x, y int) (addition int, subtraction int) {
 
 	addition = x + y
 	subtraction = x - y
+	return
+}
+
+func sliceAverage(arr []int) (average int) {
+	total := 0
+	for _, value := range arr {
+		total += value
+	}
+	average = total / len(arr)
 	return
 }
